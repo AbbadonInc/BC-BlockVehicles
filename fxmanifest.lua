9b65bbbec7b026a4283d33d3f17ab9233af4102f
BCScripts <154485417+AbbadonInc@users.noreply.github.com>
2025-02-28 15:11:02 +1000

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'AbbadonInc'
+description 'A FiveM script to block specific vehicles from spawning'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
